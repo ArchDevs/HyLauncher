8f40527590bc6e630100d7f472e2c45ffc1639d7
ronitmb <roniteam68@gmail.com>
2026-01-30 21:13:28 +0300

    update

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,31 @@
 // src/components/Navbar.tsx
 import React from "react";
+import { Gamepad2, Globe } from "lucide-react";
 
 function Navbar() {
   return (
-    <div className="absolute left-[20px] top-1/2 -translate-y-1/2 w-[48px] h-[306px] bg-[#090909]/[0.55] backdrop-blur-[12] rounded-[14px] border border-[#7C7C7C]/[0.10] flex flex-col items-center justify-center gap-16">
-      <span></span>
+    <div
+      className="
+        absolute left-[20px] top-1/2 -translate-y-1/2
+        w-[48px] h-[306px]
+        bg-[#090909]/[0.55]
+        backdrop-blur-[12px]
+        rounded-[14px]
+        border border-[#7C7C7C]/[0.10]
+        p-[16px]
+        flex flex-col
+      "
+    >
+      {/* TOP ICONS */}
+      <div className="flex flex-col items-center gap-6">
+        <button className="text-[#FFFFFF]/[0.90]">
+          <Gamepad2 size={18} />
+        </button>
+
+        <button className="text-[#FFFFFF]/[0.50]">
+          <Globe size={18} />
+        </button>
+      </div>
     </div>
   );
 }
